1f8a4ebb80c02857aa07f5d735b11cd3e8b4b6af
amanbhartiiii <amankumarbharti824112@gmail.com>
2025-01-24 10:31:04 +0530

    Imp - Allowing all GET method API for everyone without authentication

--- src/main/java/com/bharti/blog_app_api/config/SecurityConfig.java
+++ src/main/java/com/bharti/blog_app_api/config/SecurityConfig.java
@@ -6,10 +6,13 @@ import com.bharti.blog_app_api.security.JwtAuthenticationFilter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
@@ -20,9 +23,11 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+import org.springframework.util.AntPathMatcher;
 
 @Configuration
 @EnableWebSecurity
+@EnableMethodSecurity
 public class SecurityConfig {
 
     @Autowired
@@ -38,8 +43,10 @@ public class SecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
         httpSecurity
                 .csrf((csrf)-> csrf.disable())  //Disable CSRF
-                .authorizeHttpRequests((req)-> req.requestMatchers(new AntPathRequestMatcher("/api/auth/login")).permitAll()  //public api, allow for everyone
-                        .anyRequest().authenticated())   // Require authentication for other requests
+                .authorizeHttpRequests((req)-> req
+                        .requestMatchers(HttpMethod.GET, "/api/**").permitAll()   // Allow GET API without authentication
+                        .requestMatchers(new AntPathRequestMatcher("/api/auth/login")).permitAll()  //allow login api for everyone
+                        .anyRequest().authenticated())  // Require authentication for other requests
                 .exceptionHandling((ex)-> ex.authenticationEntryPoint(jwtAuthenticationEntryPoint))
                 .sessionManagement((session)-> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
 
@@ -58,3 +65,5 @@ public class SecurityConfig {
         return  NoOpPasswordEncoder.getInstance();
     }
 }
+
+// req
